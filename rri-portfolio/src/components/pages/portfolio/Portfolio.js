e48c8e574fcbb3c0d4be2ee278f689c0b72c1ec9
brake <riaconoo@icloud.com>
2022-10-03 17:07:38 +1100

    added images and styling to portfolio cards

--- rri-portfolio/src/components/pages/portfolio/Portfolio.js
+++ rri-portfolio/src/components/pages/portfolio/Portfolio.js
@@ -12,10 +12,8 @@ export default function Portfolio() {
       </div>
       <div class="row">
         <div class="column">
-          <div class="card">
-            <h3>Cocktail App</h3>
-            <p>Some text</p>
-            <p>Some text</p>
+          <div class="card cocktail-card">
+            <h3 class="card-head">Cocktail App</h3>
             <div class="project-link">
               <a class="icon" href="https://github.com/r-r-i/Project-1-Cheap-Date" target="_blank" rel="noreferrer noopener"><VscGithub /></a>
               <a class="icon" href="https://nessjade96.github.io/cheap-date/index.html" target="_blank" rel="noreferrer noopener"><FiGlobe /></a>
@@ -23,10 +21,8 @@ export default function Portfolio() {
           </div>
         </div>
         <div class="column">
-          <div class="card">
-            <h3>Workout App</h3>
-            <p>Some text</p>
-            <p>Some text</p>
+          <div class="card workout-card">
+            <h3 class="card-head">Workout App</h3>
             <div class="project-link">
               <a class="icon" href="https://github.com/r-r-i/do-you-lift" target="_blank" rel="noreferrer noopener"><VscGithub /></a>
               <a class="icon" href="https://salty-ridge-96244.herokuapp.com/" target="_blank" rel="noreferrer noopener"><FiGlobe /></a>
@@ -34,10 +30,8 @@ export default function Portfolio() {
           </div>
         </div>
         <div class="column">
-          <div class="card">
-            <h3>Text Editor</h3>
-            <p>Some text</p>
-            <p>Some text</p>
+          <div class="card text-card">
+            <h3 class="card-head">Text Editor</h3>
             <div class="project-link">
               <a class="icon" href="https://github.com/r-r-i/PWA-Text-Editor" target="_blank" rel="noreferrer noopener"><VscGithub /></a>
               <a class="icon" href="https://quiet-tor-93485.herokuapp.com/" target="_blank" rel="noreferrer noopener"><FiGlobe /></a>
@@ -45,10 +39,8 @@ export default function Portfolio() {
           </div>
         </div>
         <div class="column">
-          <div class="card">
-            <h3>Tech Blog</h3>
-            <p>Some text</p>
-            <p>Some text</p>
+          <div class="card blog-card">
+            <h3 class="card-head">Tech Blog</h3>
             <div class="project-link">
               <a class="icon" href="https://github.com/r-r-i/MVC-Blog" target="_blank" rel="noreferrer noopener"><VscGithub /></a>
               <a class="icon" href="https://agile-bastion-55945.herokuapp.com/" target="_blank" rel="noreferrer noopener"><FiGlobe /></a>
@@ -58,10 +50,8 @@ export default function Portfolio() {
       </div>
       <div class="row">
         <div class="column">
-          <div class="card">
-            <h3>Random Password</h3>
-            <p>Some text</p>
-            <p>Some text</p>
+          <div class="card password-card">
+            <h3 class="card-head">Random Password</h3>
             <div class="project-link">
               <a class="icon" href="https://github.com/r-r-i/Random-Password-Generator" target="_blank" rel="noreferrer noopener"><VscGithub /></a>
               <a class="icon" href="https://r-r-i.github.io/Random-Password-Generator/" target="_blank" rel="noreferrer noopener"><FiGlobe /></a>
@@ -69,10 +59,8 @@ export default function Portfolio() {
           </div>
         </div>
         <div class="column">
-          <div class="card">
-            <h3>Weather Dash</h3>
-            <p>Some text</p>
-            <p>Some text</p>
+          <div class="card weather-card">
+            <h3 class="card-head">Weather Dash</h3>
             <div class="project-link">
               <a class="icon" href="https://github.com/r-r-i/Weather-Dashboard" target="_blank" rel="noreferrer noopener"><VscGithub /></a>
               <a class="icon" href="https://r-r-i.github.io/Weather-Dashboard/" target="_blank" rel="noreferrer noopener"><FiGlobe /></a>
