97bba2a73a8f73d0cee68c3def0c4e1b9bdf0314
brake <riaconoo@icloud.com>
2022-10-03 18:41:50 +1100

    updated resume page, added download functionality

--- rri-portfolio/src/components/pages/resume/Resume.js
+++ rri-portfolio/src/components/pages/resume/Resume.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { AiOutlineDownload } from "react-icons/ai";
+import dlResume from '../resume/resume.txt'
 
 export default function Resume() {
   return (
     <div>
       <div class="page-container">
         <p class="page-content">
-          Below is my resume.
+          Download my resume below.
         </p>
+        <a class="resume-download"  href={dlResume} download="Resume"><AiOutlineDownload /></a>
       </div>
     </div>
   );
