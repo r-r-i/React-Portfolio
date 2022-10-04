a165713da765c52fd3e492d4458e82c4471a2895
brake <riaconoo@icloud.com>
2022-10-04 14:54:44 +1100

    added proficiencies to resume page

--- rri-portfolio/src/components/pages/resume/Resume.js
+++ rri-portfolio/src/components/pages/resume/Resume.js
@@ -11,6 +11,25 @@ export default function Resume() {
         </p>
         <a class="resume-download"  href={dlResume} download="Resume"><AiOutlineDownload /></a>
       </div>
+      <div>
+        <h1 class="page-content">Proficiencies</h1>
+        <div  class="pro-container">
+          <ul class="pro-list">
+            <li>Illustrator</li>
+            <li>InDesign</li>
+            <li>AutoDesk Maya</li>
+            <li>Cinema4D</li>
+            <li>DaVinci Resolve</li>
+          </ul>
+          <ul class="pro-list">
+            <li>HTML</li>
+            <li>C</li>
+            <li>CSS</li>
+            <li>JS</li>
+            <li>React</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
